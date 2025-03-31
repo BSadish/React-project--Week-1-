deef146dc67e77889ca9103b9a682cbb8f82ba3f
sadish panta <sadishpanta404@gmail.com>
2025-03-31 14:33:44 +0545

    Main parent for using usecontext

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
-import Hello from './components/Hello';
-import UseEffect from './components/UseEffect';
-import { Router } from 'react-router-dom';
-import ToDo from './components/ToDo';
+import React, { createContext, useState } from 'react';
+import ChildA from './components/ChildA';
 
 
+//step1: create context
+// const UserContext = createContext();
+
+//setp2: wrap all the child inside a provider
+//step3: pass the value
+//step4: consumer k andar jaake consume karlo
+
+const ThemeContext=createContext();
 const App = () => {
-  return (
-    <div>
-   {/* <Hello /> */}
-   {/* <UseEffect/> */}
-   {/* <Router/> */}
-   <ToDo/>
+  // const [user,setUser]=useState({name:"love"})
 
-   
-    </div>
+  const [theme,setTheme]=useState('light')
+  return (
+ 
+     
+
+<ThemeContext.Provider value={{theme,setTheme}}>
+  <div className={`h-100 w-100 border-2 border-black flex justify-center flex-col items-center m-10 bg-amber-200 ${theme==='light'?'bg-beign':'bg-black'}`}>
+    <ChildA/>
+  </div>
+  
+</ThemeContext.Provider>
+
+ 
   );
 };
 
 export default App;
+export {ThemeContext}
